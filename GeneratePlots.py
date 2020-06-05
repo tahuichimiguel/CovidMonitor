80d21e5ede55552987405fd8426378462168fcaf
Mikey <mikhail.fernandez.lara@gmail.com>
2020-06-04 21:35:11 -0400

    Added new features to new cases plot

--- GeneratePlots.py
+++ GeneratePlots.py
@@ -1,3 +1,4 @@
+
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
@@ -8,13 +9,15 @@ from datetime import datetime
 
 state = "PA"
 county = ["Allegheny County"]
-week_window = 5
+week_window = 6
 first_relevant_week = '3/2/20'
 
 relevant_county_names = county
 days_window = week_window*7
 
+george_floyd_protest_start = '5/30/20'
 pa_phase_yellow_date = '5/15/20'
+pa_phase_green_date = '6/5/20'
 
 def days_between(d1, d2):
     d1 = datetime.strptime(d1, "%m/%d/%Y")
@@ -51,6 +54,10 @@ plt.savefig('TotalCases.png')
 diff_df = relevant_county_covid_df.diff()
 diff_df = diff_df.tail(days_window).rename(columns = {"cases":"new-covid-cases"})
 
+rolling_avg_7_day_df = diff_df.rolling(7).mean()
+rolling_avg_5_day_df = diff_df.rolling(5).mean()
+rolling_avg_3_day_df = diff_df.rolling(3).mean()
+
 ymax = diff_df["new-covid-cases"].max()
 xmax = diff_df.shape[0]
 
@@ -59,19 +66,30 @@ diff_df.plot(y = "new-covid-cases",
              title = "New Cases in " + str(county) + " (Last " + str(days_window) + " Days)")
 plt.ylim(0,ymax+1)
 
-#hacky fix for year - fix at later point
-phase_yellow_location = days_between(pa_phase_yellow_date+str(20),diff_df.index[-1]+str(20)) - 1
+#hacky fixes for year - fix at later point
+phase_yellow_location = days_between(pa_phase_yellow_date+str(20),diff_df.index[-1]+str(20)) + 1
 plt.vlines(x = xmax-phase_yellow_location,
            ymin = 0,
            ymax = ymax+1,
            colors='y')
 
+george_floyd_start_location = days_between(george_floyd_protest_start+str(20),diff_df.index[-1]+str(20)) + 1
+plt.vlines(x = xmax-george_floyd_start_location,
+           ymin = 0,
+           ymax = ymax+1,
+           colors='black')
+
+# phase_green_location = days_between(pa_phase_green_date+str(20),diff_df.index[-1]+str(20)) + 1
+# plt.vlines(x = xmax-phase_green_location,
+#            ymin = 0,
+#            ymax = ymax+1,
+#            colors='green')
+
+plt.plot(rolling_avg_7_day_df['new-covid-cases'],'purple',label = "7-day-average")
+plt.plot(rolling_avg_5_day_df['new-covid-cases'],'red',label = "5-day-average")
+plt.plot(rolling_avg_3_day_df['new-covid-cases'],'green',label = "3-day-average")
+
+plt.legend()
 plt.grid()
 plt.savefig('NewCases.png')
-
-
-# In[ ]:
-
-
-
 
